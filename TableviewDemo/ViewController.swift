782c3615c2070abe8f8f0e2a0157c4532f8bc100
Surabhi <cstios4@gmail.com>
2017-11-12 16:59:43 +0530

    TableViewDemo

--- TableviewDemo/ViewController.swift
+++ TableviewDemo/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    var ListArray = NSArray()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ListArray = ["AirPlane Mode","Wifi","Bluetooth","Cellular","Personal Hotspot","Carrier"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ListArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = ListArray[indexPath.row] as? String
+        return cell!
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Cell Index *****\(indexPath.row)******")
+    }
+    
 }
 
